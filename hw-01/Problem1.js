2d65c28e4ced1e83ec504f0ce1c81c372e7020db
tallonch <tallonchase@gmail.com>
2024-02-04 22:26:44 -0600

    Problem 1 Complete

---
+++ hw-01/Problem1.js
@@ -0,0 +1,13 @@
+function countTo (num) {
+    for (let i = 0; i < num; i++) {
+        console.log(i);
+    }
+}
+console.log("num = 5");
+countTo(5);
+console.log("num = 1");
+countTo(1);
+console.log("num = -3");
+countTo(-3);
+console.log("num = 7");
+countTo(7);
