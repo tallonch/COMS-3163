24f17900c80cc1d102dca88f29b40a10049f3896
tallonch <tallonchase@gmail.com>
2024-03-04 15:33:28 -0600

    Project has been remade to be no-standalone

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,17 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule, NgForm } from '@angular/forms';
-import { NgFor } from "@angular/common";
-import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, HeroesComponent, FormsModule, NgFor, MessagesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'angular-tour-of-heroes';
 }
