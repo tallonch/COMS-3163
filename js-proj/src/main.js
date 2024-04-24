ee88ce4fd53a2a3c457b8d779bce46032ae91658
tallonch <tallonchase@gmail.com>
2024-04-23 22:59:34 -0500

    Working on page structure

--- js-proj/src/main.js
+++ js-proj/src/main.js
@@ -1,5 +1,30 @@
-function foo() {
-  if (true) {
-    console.log(true);
-  }
-}
+const questions = [
+  {
+    type: "multipleChoice",
+    question: "What is the capital of France?",
+    options: ["London", "Paris", "Berlin", "Rome"],
+    answer: "Paris",
+  },
+  {
+    type: "fillInTheBlank",
+    question: "The largest planet in our solar system is __________.",
+    answer: "Jupiter",
+  },
+  {
+    type: "multipleChoice",
+    question: "Where was the first example of money used?",
+    options: ["China", "Turkey", "Greece"],
+    answer: "China",
+  },
+  {
+    type: "fillInTheBlank",
+    question: "What is the longest bone the human body? __________.",
+    answer: "Femur",
+  },
+  {
+    type: "multipleChoice",
+    question: "In Greek mythology, Poseidon was the god of the sea.",
+    options: ["True", "False"],
+    answer: "True",
+  },
+];
