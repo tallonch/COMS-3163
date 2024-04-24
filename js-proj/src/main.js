b97dfe9d3e07f0ba8c3b358353e1c76156e64969
tallonch <tallonchase@gmail.com>
2024-04-24 18:50:12 -0500

    Multile choice added

--- js-proj/src/main.js
+++ js-proj/src/main.js
@@ -1,3 +1,9 @@
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerContainerElement = document.getElementById("answer-container");
+
 const questions = [
   {
     type: "multipleChoice",
@@ -28,3 +34,68 @@ const questions = [
     answer: "True",
   },
 ];
+
+let shuffledQuestions,
+  currentQuestionIndex,
+  correctAnswer,
+  totalCorrectAnswers = 0,
+  totalQuestions = questions.length;
+
+startButton.addEventListener("click", startQuiz);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
+
+function startQuiz() {
+  console.log("Started");
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
+}
+
+function setNextQuestion() {
+  resetState();
+  correctAnswer = shuffledQuestions[currentQuestionIndex].answer;
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  if (question.type == multipleChoice) {
+    question.options.forEach((option) => {
+      const button = document.createElement("button");
+      button.innerText = option;
+      button.classList.add("btn");
+      button.addEventListener("click", selectAnswer);
+      answerContainerElement.appendChild(button);
+    });
+  } else {
+  }
+  question.options.forEach((option) => {
+    const button = document.createElement("button");
+    button.innerText = option;
+    button.classList.add("btn");
+    button.addEventListener("click", selectAnswer);
+    answerContainerElement.appendChild(button);
+  });
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  if (selectedButton.innerText == correctAnswer) {
+    totalCorrectAnswers += 1;
+  }
+  console.log(totalCorrectAnswers);
+  console.log(totalQuestions);
+  nextButton.classList.remove("hide");
+}
+
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerContainerElement.firstChild) {
+    answerContainerElement.removeChild(answerContainerElement.firstChild);
+  }
+}
