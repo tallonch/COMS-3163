80224136c4bdc1a11183cb3cdaece501188d65ed
tallonch <tallonchase@gmail.com>
2024-05-07 19:34:25 -0500

    Added some divs

--- AngularQuiz/angular-quiz/src/app/app.component.ts
+++ AngularQuiz/angular-quiz/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuizComponent } from './quiz/quiz.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, QuizComponent],
 })
 export class AppComponent {
   title = 'angular-quiz';
